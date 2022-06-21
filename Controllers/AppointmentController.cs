9ea4032cd12cd4b36acf232ffc3a1e84f76eb46b
delish-khadka <np05cp4a190051@iic.edu.np>
2022-06-21 23:33:13 +0545

    Added page for admin user for doctor list dropdown

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -17,7 +17,7 @@ namespace AppointmentScheduler.Controllers
         }
         public IActionResult Index()
         {
-            _appointmentService.GetDoctorList();
+            ViewBag.DoctorList =  _appointmentService.GetDoctorList();
             return View();
         }
     }

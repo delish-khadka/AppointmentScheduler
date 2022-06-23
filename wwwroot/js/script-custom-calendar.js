b7400456f0ace80ccd946076df1783d88a54f503
delish-khadka <np05cp4a190051@iic.edu.np>
2022-06-23 22:11:07 +0545

    Updated fullcalendar js version

--- wwwroot/js/script-custom-calendar.js
+++ wwwroot/js/script-custom-calendar.js
@@ -4,20 +4,22 @@
 
 function InitializeCalendar() {
     try {
-        $('#calendar').fullCalendar({
-            timezone: false,
-            header: {
-                left: 'prev,next,today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            selectable: true,
-            editable: false,
-            select: function (event) {
-                onShowModal(event, null);
-            }
-
-        })
+        
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                headerToolbar: { 
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay'
+                },
+                selectable: true,
+                editable: false,
+                select: function (event) {
+                    onShowModal(event, null);
+                }
+            });
+            calendar.render();
     }
     catch (e) {
         alert(e);

a7594d9b1d4db62fa104ff2e19a03ad85649efc3
delish-khadka <np05cp4a190051@iic.edu.np>
2022-07-07 23:08:56 +0545

    added javascript for showing appointment details according to specific doctors

--- wwwroot/js/script-custom-calendar.js
+++ wwwroot/js/script-custom-calendar.js
@@ -7,13 +7,14 @@ $(document).ready(function () {
 
     InitializeCalendar();
 });
+var calendar;
 
 function InitializeCalendar() {
     try {
         
         var calendarEl = document.getElementById('calendar');
         if (calendarEl != null) {
-            var calendar = new FullCalendar.Calendar(calendarEl, {
+            calendar = new FullCalendar.Calendar(calendarEl, {
                 initialView: 'dayGridMonth',
                 headerToolbar: {
                     left: 'prev,next,today',
@@ -153,4 +154,8 @@ function getEventDetailsByEventId(info) {
             $.notify("Error", "error")
         }
     });
+}
+
+function onDoctorChange() {
+    calendar.refetchEvents();
 }
